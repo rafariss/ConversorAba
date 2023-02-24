2baa8ac651e042abbba88ed85aa2a77c3ea5dc15
Rafael D Rissmann <rafariss@gmail.com>
2023-02-24 15:30:41 -0300

    Inserção de labels faltantes, ajustes e conversões

--- ConversorAba/MainWindow.xaml.cs
+++ ConversorAba/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -27,7 +29,69 @@ namespace ConversorAba
 
         private void tb1_TextChanged(object sender, TextChangedEventArgs e)
         {
-            lb2.Content = tb1.Text;
+            lbAbatrack.Content = tbAbatrack.Text;
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            string aba = tbAbatrack.Text;
+            int toBase = 16;
+            long valorint = long.Parse(aba);
+            lbSerial.Content =  Convert.ToString(valorint, toBase);
+            string hexa1 = Convert.ToString(valorint, toBase);
+            string hexa = hexa1.TrimStart();
+            if(hexa.Length > 8 && hexa.Length < 10)
+            {
+                string parte1 = hexa.Substring(0, 5);
+                string parte2 = hexa.Substring(5, 4);
+
+                string part1_conv = parte1.Substring(3, 2);
+                string part2_conv = parte2.Substring(0, 4);
+
+                int dec1 = Convert.ToInt32(part1_conv, toBase);
+                int dec2 = Convert.ToInt32(part2_conv, toBase);
+
+                lbWiegand.Content = dec1 + "-" + dec2;
+
+            }
+            if (hexa.Length >= 10)
+            {
+                string parte1 = hexa.Substring(0, 6);
+                string parte2 = hexa.Substring(6, 4);
+
+                string part1_conv = parte1.Substring(4, 2);
+                string part2_conv = parte2.Substring(0, 4);
+
+                int dec1 = Convert.ToInt32(part1_conv, toBase);
+                int dec2 = Convert.ToInt32(part2_conv, toBase);
+
+                lbWiegand.Content = dec1 + "-" + dec2;
+
+
+            }
+            else
+            {
+                string parte1 = hexa.Substring(0, 2);
+                string parte2 = hexa.Substring(2, 4);
+
+                string part1_conv = parte1.Substring(0, 2);
+                string part2_conv = parte2.Substring(0, 4);
+
+                int dec1 = Convert.ToInt32(part1_conv, toBase);
+                int dec2 = Convert.ToInt32(part2_conv, toBase);
+
+                lbWiegand.Content = dec1 + "-" + dec2;
+            }
+           
+
+
+
+
+
+
+
+
+
         }
     }
 }
